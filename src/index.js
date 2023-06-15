06f83e94ea55aa636177c2e324ab33a166d42370
kristian <kristian.spasov27@gmail.com>
2023-06-15 22:16:11 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -15,7 +16,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(5000, console.log("Server is listening on port 5000..."));
